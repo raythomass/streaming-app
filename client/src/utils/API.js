7ba092a39eee9ae8410bfbbc1f7335bf79a5463a
Ray Thomas <rthomas5678@gmail.com>
2024-04-23 15:53:21 -0700

    working on fetching data from API.js instead of inside each individual file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,27 @@
 import axios from 'axios';
 const apiKey = import.meta.env.VITE_API_KEY;
+const bearerKey = import.meta.env.VITE_BEARER_KEY;
+
+const fetchShows = async () => {
+    const url = 'https://api.themoviedb.org/3/trending/tv/day?language=en-US';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${bearerKey}`
+        }
+    };
+
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json.results))
+    .catch(err => console.error('error:' + err));
+
+};
+
+// const fetchShows = async () => {
+//     return axios.get(`https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`)
+// }
 
 const fetchSingleMovie = async (id) => {
     return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
@@ -12,5 +34,6 @@ const fetchSingleShow = async (id) => {
 
 export default {
     fetchSingleMovie,
+    fetchShows,
     fetchSingleShow
 }
